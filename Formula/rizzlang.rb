5becc0cb876be1d16ed481c02ae504a34d6aea97
Sriyush <asish.ayush.sri@gmail.com>
2025-09-03 15:14:58 +0530

    making it directly from src

--- Formula/rizzlang.rb
+++ Formula/rizzlang.rb
@@ -5,9 +5,11 @@ class Rizzlang < Formula
   # version "1.0.14"
   sha256 "f76dee4dcb874fbc8ea6dab094ee60771f8c2432fa3b841631c989ba8db1f78f"
   license "MIT"
-  def install
-    bin.install "rizz"
-  end
+def install
+  system "cmake", ".", *std_cmake_args
+  system "make"
+  bin.install "rizz"
+end
 
   test do
     system "#{bin}/rizz", "--version"
