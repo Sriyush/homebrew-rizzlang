963fff739c23ff856ba192f45d13be0209c4e02c
Ayush Srivastava <108729564+Sriyush@users.noreply.github.com>
2025-09-03 11:58:17 +0530

    Update rizzlang.rb

--- rizzlang.rb
+++ rizzlang.rb
@@ -2,8 +2,8 @@ class Rizzlang < Formula
   desc "Rizz++ - small GenZ-style toy language"
   homepage "https://github.com/Sriyush/RizzLang"
   url "https://github.com/Sriyush/RizzLang/releases/download/v1.0.12/rizz"
-  sha256 "<SHA256>"
-  version "VERSION"
+  sha256 "fdcfd377e5168396b1dca7505bad2a9e0568ced53ff1896896d621dc8811addc"
+  version "1.0.12"
 
   def install
     bin.install "rizz"
