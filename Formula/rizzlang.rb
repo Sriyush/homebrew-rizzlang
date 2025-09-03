9f4da9df9a3c299f70df23d3e1afcb397f829c9b
Sriyush <asish.ayush.sri@gmail.com>
2025-09-03 17:43:20 +0530

    changes in ruby script

--- Formula/rizzlang.rb
+++ Formula/rizzlang.rb
@@ -1,12 +1,21 @@
 class Rizzlang < Formula
   desc "Rizz++ - small GenZ-style toy language"
   homepage "https://github.com/Sriyush/RizzLang"
-  url "https://github.com/Sriyush/RizzLang/releases/download/v1.0.15/rizz"
-  # version "1.0.14"
-  sha256 "f76dee4dcb874fbc8ea6dab094ee60771f8c2432fa3b841631c989ba8db1f78f"
+  if OS.mac?
+    url "https://github.com/Sriyush/RizzLang/releases/download/v1.0.16/rizz-macos"
+    sha256 "f76dee4dcb874fbc8ea6dab094ee60771f8c2432fa3b841631c989ba8db1f78f"
+  elsif OS.linux?
+    url "https://github.com/Sriyush/RizzLang/releases/download/v1.0.16/rizz-linux"
+    sha256 "0042bc8eebddc4ef9578aec85e3f8cd0f30c3e69caf6c3f4a3a17431330750d0"
+  end
   license "MIT"
+
   def install
-    bin.install "rizz"
+    if OS.mac?
+      bin.install "rizz-macos" => "rizz"
+    elsif OS.linux?
+      bin.install "rizz-linux" => "rizz"
+    end
   end
 
   test do
