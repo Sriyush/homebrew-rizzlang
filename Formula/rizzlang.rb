26835903e85239c4f90ef29fc79c4616eb32fc3b
Sriyush <asish.ayush.sri@gmail.com>
2025-09-03 13:47:34 +0530

    adding license

--- Formula/rizzlang.rb
+++ Formula/rizzlang.rb
@@ -2,9 +2,9 @@ class Rizzlang < Formula
   desc "Rizz++ - small GenZ-style toy language"
   homepage "https://github.com/Sriyush/RizzLang"
   url "https://github.com/Sriyush/RizzLang/releases/download/v1.0.14/rizz"
+  # version "1.0.14"
   sha256 "f76dee4dcb874fbc8ea6dab094ee60771f8c2432fa3b841631c989ba8db1f78f"
-  version "1.0.14"
-
+  license "MIT"
   def install
     bin.install "rizz"
   end
