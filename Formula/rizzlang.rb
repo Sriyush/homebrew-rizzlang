9a9aa339ffced3ebd4987cead3857caa900b6dca
Sriyush <asish.ayush.sri@gmail.com>
2025-09-03 15:17:07 +0530

    changing the ruby fike

--- Formula/rizzlang.rb
+++ Formula/rizzlang.rb
@@ -5,11 +5,12 @@ class Rizzlang < Formula
   # version "1.0.14"
   sha256 "f76dee4dcb874fbc8ea6dab094ee60771f8c2432fa3b841631c989ba8db1f78f"
   license "MIT"
-def install
-  system "cmake", ".", *std_cmake_args
-  system "make"
-  bin.install "rizz"
-end
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    bin.install "rizz"
+  end
+
 
   test do
     system "#{bin}/rizz", "--version"
