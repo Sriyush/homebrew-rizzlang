2b109b6896291e46a99570c2d0e3089d189806dc
Sriyush <asish.ayush.sri@gmail.com>
2025-09-04 11:30:14 +0530

    fix

--- Formula/rizzlang.rb
+++ Formula/rizzlang.rb
@@ -14,7 +14,7 @@ class Rizzlang < Formula
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", "..", *std_cmake_args
     system "make", "install"
   end
 
