2c8a763a5505da6e5fd556150da05173503b3571
Sriyush <asish.ayush.sri@gmail.com>
2025-09-04 12:54:59 +0530

    fix

--- Formula/rizzlang.rb
+++ Formula/rizzlang.rb
@@ -5,12 +5,19 @@ class Rizzlang < Formula
   sha256 "d9af95a6cbe8cf2ae9078bb0315acefb6e6c6569da305594a3e32c9197bfbef4"
   license "MIT"
 
+  bottle do
+  sha256 cellar: :any_skip_relocation, arm64_monterey: "f76dee4dcb874fbc8ea6dab094ee60771f8c2432fa3b841631c989ba8db1f78f"
+  sha256 cellar: :any_skip_relocation, x86_64_linux:   "0042bc8eebddc4ef9578aec85e3f8cd0f30c3e69caf6c3f4a3a17431330750d0"
+  end
+
+  depends_on "gcc"
   depends_on "cmake" => :build
-  depends_on "gcc" => :build
 
   def install
     ENV["CC"] = Formula["gcc"].opt_bin/"gcc-15"
     ENV["CXX"] = Formula["gcc"].opt_bin/"g++-15"
+
+    ENV["LDFLAGS"] = "-Wl,-rpath,#{gcc.opt_lib}"
 
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
